186c7d73e9d0283a21d4640c2e9eb5d2ca3b1b7e
cecilioAlvarez <cmac@arquitecturajava.com>
2023-07-21 10:47:12 +0200

    sass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { C1Component } from './c1/c1.component';
+import { C2Component } from './c2/c2.component';
+import { C3Component } from './c3/c3.component';
+import { C4Component } from './c4/c4.component';
+import { C5Component } from './c5/c5.component';
+import { C6Component } from './c6/c6.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    C1Component,
+    C2Component,
+    C3Component,
+    C4Component,
+    C5Component,
+    C6Component
   ],
   imports: [
     BrowserModule
